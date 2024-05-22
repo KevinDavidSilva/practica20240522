3f497343ac3f75a380b5d85d30da5f64766439b7
KevinSilva <kevinsilvadb503@gmail.com>
2024-05-22 10:40:33 -0600

    la clase futbolista tiene sus propiedades y metodos

--- src/Futbolista.java
+++ src/Futbolista.java
@@ -1,3 +1,44 @@
-public class Futbolista {
+public class Futbolista extends Persona {
+    private int dorsal;
+    private String demarcacion;
 
+    public Futbolista(int id, String nombre, String apellidos, int edad, int dorsal, String demarcacion) {
+        super(id, nombre, apellidos, edad);
+        this.dorsal = dorsal;
+        this.demarcacion = demarcacion;
+    }
+
+    public int getDorsal() {
+        return dorsal;
+    }
+
+    public String getDemarcacion() {
+        return demarcacion;
+    }
+
+    @Override
+    public void concentrarse() {
+        System.out.println("El futbolista se concentra...");
+    }
+
+    @Override
+    public void viajar() {
+        System.out.println("El futbolista viaja...");
+    }
+
+    public void jugarPartido() {
+        System.out.println("El futbolista juega un partido...");
+    }
+
+    public void entrenar() {
+        System.out.println("El futbolista entrena...");
+    }
+
+    @Override
+    public String toString() {
+        return "Futbolista{" +
+                "dorsal=" + dorsal +
+                ", demarcacion='" + demarcacion + '\'' +
+                "} " + super.toString();
+    }
 }
