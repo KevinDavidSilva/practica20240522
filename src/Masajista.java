c2b693fb7113d19e9f928404760044d8350eda7d
KevinSilva <kevinsilvadb503@gmail.com>
2024-05-22 10:44:01 -0600

    la clase masajista tiene sus propiedades y metodos

--- src/Masajista.java
+++ src/Masajista.java
@@ -1,3 +1,40 @@
-public class Masajista {
+public class Masajista extends Persona {
+    private String titulacion;
+    private int aniosExperiencia;
 
+    public Masajista(int id, String nombre, String apellidos, int edad, String titulacion, int aniosExperiencia) {
+        super(id, nombre, apellidos, edad);
+        this.titulacion = titulacion;
+        this.aniosExperiencia = aniosExperiencia;
+    }
+
+    public String getTitulacion() {
+        return titulacion;
+    }
+
+    public int getAniosExperiencia() {
+        return aniosExperiencia;
+    }
+
+    @Override
+    public void concentrarse() {
+        System.out.println("El masajista se concentra:");
+    }
+
+    @Override
+    public void viajar() {
+        System.out.println("El masajista viaja:");
+    }
+
+    public void darMasaje() {
+        System.out.println("El masajista da un masaje:");
+    }
+
+    @Override
+    public String toString() {
+        return "Masajista{" +
+                "titulacion='" + titulacion + '\'' +
+                ", aniosExperiencia=" + aniosExperiencia +
+                "} " + super.toString();
+    }
 }
