6fb745e85cc20e681d79104d97195263a23933ef
KevinSilva <kevinsilvadb503@gmail.com>
2024-05-22 10:54:45 -0600

    la clase main ya tiene la funcionalidad de mostrar la informacion del fulbolista del entregador y el masajista

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Futbolista futbolista = new Futbolista(1, "kevin", "silva", 19, 8, "portero");
+        Entrenador entrenador = new Entrenador(2, "ramdaran", "guardado", 20, "AL-001");
+        Masajista masajista = new Masajista(3, "benito", "rivera", 25, "Licenciado", 30);
+
+        System.out.println(futbolista);
+        System.out.println(entrenador);
+        System.out.println(masajista);
+
+        futbolista.jugarPartido();
+        entrenador.dirigirEntrenamiento();
+        masajista.darMasaje();
+
+        futbolista.concentrarse();
+        futbolista.viajar();
+        entrenador.concentrarse();
+        entrenador.viajar();
+        masajista.concentrarse();
+        masajista.viajar();
     }
 }
