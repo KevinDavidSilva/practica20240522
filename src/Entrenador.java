463432be3bceb5637a0bfe0c3dc35f6ea807fe01
KevinSilva <kevinsilvadb503@gmail.com>
2024-05-22 10:37:31 -0600

    se creo la clase padre persona

--- src/Entrenador.java
+++ src/Entrenador.java
@@ -1,3 +1,3 @@
-public class Entrenador {
+public class Entrenador  {
 
 }
---
+++ src/Persona.java
@@ -0,0 +1,46 @@
+public abstract class Persona {
+    private int id;
+    private String nombre;
+    private String apellidos;
+    private int edad;
+
+    public Persona() {
+    }
+
+    public Persona(int id, String nombre, String apellidos, int edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public abstract void concentrarse();
+
+    public abstract void viajar();
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
+}
